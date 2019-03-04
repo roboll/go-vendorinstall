921b89b47031fdc1999491dcf03e78eeb16ed56d
Lubos Jirak <lubos.jirak@deutsche-boerse.com>
2019-03-04 10:24:19 +0100

    adding GOPATH, needed for go version 1.12

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func main() {
 	os.Setenv("PATH", fmt.Sprintf("%s%s%s", gobin, string(os.PathListSeparator), os.Getenv("PATH")))
 	defer os.Setenv("PATH", oldpath)
 
-	env := []string{fmt.Sprintf("PATH=%s", path), fmt.Sprintf("GOPATH=%s", gopath), fmt.Sprintf("GOBIN=%s", gobin)}
+	env := []string{fmt.Sprintf("PATH=%s", path), fmt.Sprintf("GOPATH=%s", gopath), fmt.Sprintf("GOBIN=%s", gobin), fmt.Sprintf("GOCACHE=%s", gopath)}
 	args := append([]string{"install"}, packages...)
 	if out, err := doexec("go", gopath, args, env); err != nil {
 		print(string(out))
